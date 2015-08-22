7a4a50ba48d8f6beb04ccc01b7d4822d8dfb5313
Mark Asfour <mj4sparrow@gmail.com>
2015-08-22 01:13:51 -0700

    can encrypt a sentence

--- src/encrypt.cpp
+++ src/encrypt.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 using namespace std;
 
 vector <char> characters = {'a', 'b', 'c', 'd', 'e',
@@ -20,7 +21,8 @@ vector <char> characters = {'a', 'b', 'c', 'd', 'e',
 							'-', '_', '=', '+', '[',
 							'{', ']', '}', '\\', '|',
 							';', ':', '\'', '"', ',',
-							'<', '.', '>', '/', '?'};
+							'<', '.', '>', '/', '?',
+							' '};
 
 int get_d (int e, int t)
 {
@@ -47,21 +49,49 @@ int get_d (int e, int t)
 	}
 }
 
+int find_index (char a)
+{
+	for (int i = 0; i < characters.size(); i++)
+	{
+		if (a == characters.at(i))
+		{
+			return i;
+		}
+	}
+}
+
+int encrypt (int m, int e, int n)
+{
+	int c = 1;
+	for (int i = 0; i < (e / 2); i++)
+	{
+		c *= ((m * m) % n);
+		c %= n;
+		//cout << "c = " << c << endl;
+	}
+
+	if (e % 2 == 1)
+	{
+		c *= m;
+	}
+	return c;
+}
+
 int main(int argc, const char * argv[]) 
 {
 	int p, q, n, totient, e, d;
 	do {
-	cout << "please enter a prime number p" << endl;
-	cin >> p; //assumes p is prime
-	cout << "please enter different prime number q" << endl;
-	cin >> q; //assumes q is prime and different than p
-	
-	n = p * q;
-	cout << "n = " << n << endl;
-	if (n <= characters.size())
-	{
-		cout << "please choose larger p and q" << endl;
-	}
+		cout << "please enter a prime number p" << endl;
+		cin >> p; //assumes p is prime
+		cout << "please enter different prime number q" << endl;
+		cin >> q; //assumes q is prime and different than p
+		
+		n = p * q;
+		cout << "n = " << n << endl;
+		if (n <= characters.size())
+		{
+			cout << "please choose larger p and q" << endl;
+		}
 	} while (n <= characters.size());
 
 	totient = (p - 1) * (q - 1);
@@ -73,5 +103,19 @@ int main(int argc, const char * argv[])
 	d = get_d(e, totient);
 	cout << "d = " << d << endl;
 
+	string input;
+	cout << "Enter a sentence you would like to encrypt: " << endl;
+	cin.ignore();
+	getline(cin, input);
+
+	string encrypted;
+	for (int i = 0; i < input.size(); i++)
+	{
+		int m = find_index(input.at(i));
+		//cout << "m = " << m + 2 << endl;
+		encrypted += to_string(encrypt(m + 2, e, n)); //m+2 so that the number 0 or 1 is never exponentiated 
+		//cout << "encrypted = " << encrypted << endl;
+		encrypted += " ";
+	}
 	return 0;
 }
